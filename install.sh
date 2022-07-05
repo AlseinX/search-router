f4d3e1668599342a30ec891abec42b8af71a64e7
AlseinX <xyh951115@live.com>
2022-07-05 16:43:07 +0800

    fix service

--- install.sh
+++ install.sh
@@ -12,6 +12,7 @@ fi
 mkdir -p /usr/local/search-router/
 mv ./target/release/search-router /usr/local/search-router/
 cp ./search-router.service /usr/local/search-router/
+ln -s /usr/local/search-router/search-router.service /etc/systemd/system/search-router.service
 ln -s /usr/local/search-router/search-router.service /etc/systemd/system/multi-user.target.wants/search-router.service
 systemctl enable search-router
 systemctl restart search-router
